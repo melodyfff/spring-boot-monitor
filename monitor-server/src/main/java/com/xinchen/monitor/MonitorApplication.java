d4514d157144deb10a92e416989cae08e2fd6d13
melodyfff <307208327@qq.com>
2019-05-21 23:55:53 +0800

    :smile: spring-boot-admin-server

---
+++ monitor-server/src/main/java/com/xinchen/monitor/MonitorApplication.java
@@ -0,0 +1,17 @@
+package com.xinchen.monitor;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@EnableAdminServer
+public class MonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorApplication.class, args);
+    }
+
+}
